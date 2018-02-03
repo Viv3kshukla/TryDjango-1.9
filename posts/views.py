e9c4b4e876e7b808d4011e05ea1f9acc448a7859
Vivek Shukla <vivekshukla@iiitdmj.ac.in>
2018-02-03 10:49:00 +0530

    added templates

--- posts/views.py
+++ posts/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 def posts_home(request):
 
-    return HttpResponse("<h1> Hello Views</h1>")
+    return render(request,'index.html',{})
 
 def posts_create(request):
 
